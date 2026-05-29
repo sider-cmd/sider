72d9c38587a8459e3a4381a24cd5367b3104ebb5
sider-cmd <lfclu0414@gmail.com>
2026-05-29 13:43:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.get('*', (req, res) => {
 // =================【4. 啟動伺服器】=================
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);      
 });
