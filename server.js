7c875cf37ab619c8c5f64d932a4a31f89b15bd4f
sider-cmd <lfclu0414@gmail.com>
2026-05-29 22:43:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ if (/^\d{4}$/.test(stockId)) {
     const latest = data.data[data.data.length - 1];
 
     const stockReply = `
-📈 台積電 (2330)
+📈 股票代號：${stockId}
 
 收盤價：${latest.close} 元
 開盤價：${latest.open} 元
